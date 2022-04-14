0669570911fc9ec7d431aaa99ec3b318ba3f5d58
Dayana Ismailova <dayana530807@gmail.com>
2022-04-14 17:11:55 +0300

    fix redirect_uri

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -24,7 +24,9 @@
         class="col-12 col-sm-6"
       >
         <template #footer>
-          <button @click="exportConsole" class="btn btn-primary">Export to console</button>
+          <button @click="exportConsole" class="btn btn-primary">
+            Export to console
+          </button>
         </template>
       </ListColumn>
     </div>
@@ -42,7 +44,8 @@ import { getUserInfo, deleteUserInfo } from "../utils";
 const clientId = "8136588";
 const protectedKey = "DpJYSIP4hegE1E5j6wvJ";
 const redirectUri =
-  window.location.origin + "/vk-oauth2" || "https://oauth.vk.com/blank.html";
+  window.location.origin + "/friends-app/vk-oauth2" ||
+  "https://oauth.vk.com/blank.html";
 const authUrl = `https://oauth.vk.com/authorize?client_id=${clientId}&display=page&redirect_uri=${redirectUri}&scope=friends&response_type=token&v=5.81`;
 
 const userInfo = ref(getUserInfo());
@@ -74,19 +77,16 @@ function exportConsole() {
 async function getFriends() {
   if (!userInfo.value) return [];
 
-  const { data } = await axios.get(
-    `https://api.vk.com/method/friends.get`,
-    {
-      params: {
-        v: "5.131",
-        access_token: userInfo.value.access_token,
-        user_id: userInfo.value.user_id,
-        order: "name",
-        // count: 5,
-        fields: "first_name,last_name,photo_100,nickname",
-      },
-    }
-  );
+  const { data } = await axios.get(`https://api.vk.com/method/friends.get`, {
+    params: {
+      v: "5.131",
+      access_token: userInfo.value.access_token,
+      user_id: userInfo.value.user_id,
+      order: "name",
+      // count: 5,
+      fields: "first_name,last_name,photo_100,nickname",
+    },
+  });
 
   return data?.response?.items || [];
 }
